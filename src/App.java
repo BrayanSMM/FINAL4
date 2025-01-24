0ccb806dbc8e45f6b4e5f9f0558eae3b8f26ee82
BrayanSMM <brayanstivenmolinamacias30@gmail.com>
2025-01-24 15:45:12 +0100

    Se agrega el mensaje en el main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("Primer mensaje");
         System.out.println("Hello, World!");
     }
 }
